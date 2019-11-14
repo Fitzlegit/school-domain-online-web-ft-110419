97267bbf5ddc2eeb0b01a76edfdc94a6fc8863c9
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-11-14 22:23:10 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,6 +10,14 @@ class School
   
   def add_student(name, grade)
     
+    if roster[grade]
+      roster[grade] << name 
+    else 
+      roster[grade] = []
+      roster[grade] << name 
+    end 
+
+    
     roster[grade] ||= []
     #check to seee if there's a value here, if not make it an array. 
     #if there is, don't do anything
